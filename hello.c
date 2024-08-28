e12567de0ba145eb3cdd332cc8ca3b9632f2b16a
widyan <walmothafar2@huskers.unl.edu>
2024-08-28 16:22:10 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Widyan
+ *         walmothafar@huskers.unl.edu
+ * Date: 2024/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("widyan\n");
+
+  return 0;
+}
