d2018ec468681599160531d1d5864178a1d50bd3
widyan <walmothafar2@huskers.unl.edu>
2024-08-29 13:21:41 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,1 +1,17 @@
+/**
+ * Author: Widyan
+ * walmothafar2@huskers.unl.edu
+ * Date: 2024/08/29
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
 
+ int main(int argc, char **argv) {
+
+    printf("Widyan\n");
+
+    return 0;
+ }
